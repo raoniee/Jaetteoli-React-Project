92a33baa9ce14a4e86419bc6f1b30d1b84be981b
Anwooseong <aws5624@naver.com>
2023-06-29 22:56:25 +0900

    코드 리팩토링(1)

--- src/components/UI/Input.js
+++
@@ -1,9 +0,0 @@
-import classes from './Input.module.css'
-
-const Input = (props) => {
-    return (
-        <input type="text" placeholder={props.placeholder} />
-    )
-}
-
-export default Input;
