95f7bd3da1bb19c6384cb52af1270439be3943fb
한지은 <hjn510450@gmail.com>
2023-06-30 14:30:34 +0900

    초기화면 수정(경로 수정)

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -2,15 +2,15 @@ import Download from './Download';
 import style from './Home.module.css'
 import HomeDescription from './HomeDescription';
 import Login from './Login';
-import home_bg1 from '../../image/home_bg1.png'
-import home_bg2 from '../../image/home_bg2.png'
-import home_bg3 from '../../image/home_bg3.png'
-import home_bg4 from '../../image/home_bg4.png'
-import home_bg5 from '../../image/home_bg5.png'
+import home_bg1 from '../../assets/images/home_bg1.png'
+import home_bg2 from '../../assets/images/home_bg2.png'
+import home_bg3 from '../../assets/images/home_bg3.png'
+import home_bg4 from '../../assets/images/home_bg4.png'
+import home_bg5 from '../../assets/images/home_bg5.png'
 import { useEffect, useState } from 'react';
 
 const Home = () => {
-    const images = [home_bg1, home_bg2, home_bg3, home_bg4, home_bg5]; // 여러 개의 이미지 경로
+    const images = [home_bg1, home_bg2, home_bg3, home_bg4, home_bg5]; 
     const [currentImage, setCurrentImage] = useState(0);
 
     useEffect(() => {
