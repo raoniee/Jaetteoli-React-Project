6ca175f2d0a5088c03ed999649bb0cd431b60d75
Anwooseong <aws5624@naver.com>
2023-07-06 21:30:38 +0900

    Footer import

--- src/component/login/PasswordNew.jsx
+++ src/component/login/PasswordNew.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../header/Header";
 import style from "./PasswordNew.module.css";
+import Footer from "../footer/Footer";
 
 export default function PasswordNew() {
   return (
