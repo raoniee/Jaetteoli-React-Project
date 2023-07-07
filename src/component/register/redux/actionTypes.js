b9b51ef39bdb440698b95eadbed0b730fd1c0298
vavoya <128780530+vavoya@users.noreply.github.com>
2023-07-08 07:49:13 +0900

    Delete src/component/register/redux directory

--- src/component/register/redux/actionTypes.js
+++
@@ -1,6 +0,0 @@
-
-export const STORE_INFO = 'storeInfo';
-
-export const MENU_INFO = 'menuInfo';
-
-export const ORIGIN_INFO = 'originInfo';
--- src/component/register/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux';
-import counterReducer from './counterReducer';
-
-const store = createStore(counterReducer);
-
-export default store;
