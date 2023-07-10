d3134854e040c2534837c594e883773b2c4e282f
vavoya <128780530+vavoya@users.noreply.github.com>
2023-07-10 23:34:12 +0900

    경로 수정

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './auth'
-import menuRegisteringReducer from '../component/register/redux/menuRegisteringSlice'
+import menuRegisteringReducer from "./menuRegisteringSlice";
 
 export default configureStore({
     reducer: {
