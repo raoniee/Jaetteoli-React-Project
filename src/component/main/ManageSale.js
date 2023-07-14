d7d3f2fe92df1a642da2ef56cf62ae5101e8bf80
한지은 <hjn510450@gmail.com>
2023-07-14 19:29:47 +0900

    판매 관리 페이지 api(오늘 총 매출, 시간대 별 매출, 요일별 매출) 연결

--- src/component/main/ManageSale.js
+++ src/component/main/ManageSale.js
@@ -1,75 +1,177 @@
-import { PureComponent } from 'react';
+import { PureComponent, useState, useEffect } from 'react';
+import { getCookieToken } from "../../store/common/Cookie";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, BarChart, Bar, Cell, PieChart, Pie, Sector } from 'recharts';
 import style from './ManageSale.module.css'
 import Header from '../header/Header'
 import SideBar from '../sidebar/SideBar'
 
 const ManageSale = () => {
-    const timeData = [
-        {
-            name: '16시',
-            오늘: 4000,
-            어제: 2400,
-        },
-        {
-            name: '17시',
-            오늘: 3000,
-            어제: 1398,
-        },
-        {
-            name: '18시',
-            오늘: 2000,
-            어제: 9800,
-        },
-        {
-            name: '19시',
-            오늘: 2780,
-            어제: 3908,
-        },
-        {
-            name: '20시',
-            오늘: 1890,
-            어제: 4800,
-        },
-    ];
+    // 오늘 총 매출
+    const [todayTotalSales, setTodayTotalSales] = useState(null);
 
-    const dayData = [
-        {
-            name: '월',
-            이번주: 4000,
-            지난주: 2400,
-        },
-        {
-            name: '화',
-            이번주: 3000,
-            지난주: 1398,
-        },
-        {
-            name: '수',
-            이번주: 2000,
-            지난주: 9800,
-        },
-        {
-            name: '목',
-            이번주: 2780,
-            지난주: 3908,
-        },
-        {
-            name: '금',
-            이번주: 1890,
-            지난주: 4800,
-        },
-        {
-            name: '토',
-            이번주: 2580,
-            지난주: 4800,
-        },
-        {
-            name: '일',
-            이번주: 7430,
-            지난주: 3490,
-        },
-    ];
+    // 요일 별 매출
+    const [dayOfThisWeekSales, setDayOfThisWeekSales] = useState(null);
+    const [dayOfLastWeekSales, setDayOfLastWeekSales] = useState(null);
+    const [dayData, setDayData] = useState([
+        { name: '월', 이번주: 0, 지난주: 0 },
+        { name: '화', 이번주: 0, 지난주: 0 },
+        { name: '수', 이번주: 0, 지난주: 0 },
+        { name: '목', 이번주: 0, 지난주: 0 },
+        { name: '금', 이번주: 0, 지난주: 0 },
+        { name: '토', 이번주: 0, 지난주: 0 },
+        { name: '일', 이번주: 0, 지난주: 0 },
+    ]);
+
+    // 시간 별 매출
+    const [todayHourlySales, setTodayHourlySales] = useState(null);
+    const [yesterdayHourlySales, setYesterdayHourlySales] = useState(null);
+    const [openTime, setOpenTime] = useState(null);
+    const [closeTime, setCloseTime] = useState(null);
+    const [timeData, setTimeData] = useState(null);
+
+    const token = getCookieToken();
+
+    async function getTodayTotalsales() {
+        const requestOptions = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-ACCESS-TOKEN': token,
+            },
+        };
+        try {
+            const response = await fetch("https://www.insung.shop/jat/sales/today", requestOptions);
+            const data = await response.json();
+
+            if (!data.isSuccess) {
+                console.log(data.message);
+                return;
+            }
+
+            return data.result.todayTotalSales;
+        } catch (error) {
+            console.log('서버가 아직 안켜져있습니다.')
+            console.log(error)
+        }
+    }
+
+    async function getDayOfWeekSales() {
+        const requestOptions = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-ACCESS-TOKEN': token,
+            },
+        };
+        try {
+            const response = await fetch("https://www.insung.shop/jat/sales/weekday", requestOptions);
+            const data = await response.json();
+
+            if (!data.isSuccess) {
+                console.log(data.message);
+                return;
+            }
+
+            return data.result;
+        } catch (error) {
+            console.log('서버가 아직 안켜져있습니다.')
+            console.log(error)
+        }
+    }
+
+    async function getHourlySales() {
+        const requestOptions = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-ACCESS-TOKEN': token,
+            },
+        };
+        try {
+            const response = await fetch("https://www.insung.shop/jat/sales/yesterday", requestOptions);
+            const data = await response.json();
+
+            if (!data.isSuccess) {
+                console.log(data.message);
+                return;
+            }
+
+            return data.result;
+        } catch (error) {
+            console.log('서버가 아직 안켜져있습니다.')
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getTodayTotalsales()
+            .then(todayTotalSales => {
+                setTodayTotalSales(todayTotalSales);
+            })
+        getDayOfWeekSales()
+            .then(result => {
+                setDayOfThisWeekSales(result.thisWeek);
+                setDayOfLastWeekSales(result.lastWeek);
+            })
+        getHourlySales()
+            .then(result => {
+                setTodayHourlySales(result.salesToday);
+                setYesterdayHourlySales(result.salesYesterday);
+                setOpenTime(result.storeOpen.split(':')[0]);
+                setCloseTime(result.storeClose.split(':')[0]);
+            })
+    }, []);
+
+    useEffect(() => {
+        if (dayOfThisWeekSales && dayOfLastWeekSales) {
+            const updatedDayData = dayData.map((dayItem, index) => {
+                const thisWeekTotalPrice = dayOfThisWeekSales.find(sale => sale.day === index)?.totalPrice || 0;
+                const lastWeekTotalPrice = dayOfLastWeekSales.find(sale => sale.day === index)?.totalPrice || 0;
+                return { ...dayItem, 이번주: thisWeekTotalPrice, 지난주: lastWeekTotalPrice };
+            });
+            setDayData(updatedDayData);
+        }
+    }, [dayOfThisWeekSales, dayOfLastWeekSales]);
+
+    useEffect(() => {
+        if (openTime && closeTime) {
+            const startTime = parseInt(openTime, 10); // 오픈시간을 정수형으로 변환
+            const endTime = parseInt(closeTime, 10); // 클로즈시간을 정수형으로 변환
+        
+            // timeData 생성
+            const updatedTimeData = [];
+            for (let i = startTime; i <= endTime; i++) {
+              updatedTimeData.push({
+                name: `${i}시`,
+                오늘: 0,
+                어제: 0,
+              });
+            }
+            setTimeData(updatedTimeData);
+          }
+    }, [openTime, closeTime]);
+
+    useEffect(() => {
+        if (todayHourlySales && yesterdayHourlySales, timeData) {
+          const updatedTimeData = [...timeData]; // timeData 복사하여 수정할 배열 생성
+      
+          todayHourlySales.forEach(sale => {
+            const { time, totalSalesPriceInTime } = sale;
+            const hour = Number(time.split(' ')[1]); // 시간 부분 추출
+            const index = hour - 10; // 시간에 해당하는 인덱스 계산
+            updatedTimeData[index].오늘 = totalSalesPriceInTime; // 해당 시간의 오늘 매출액 업데이트
+          });
+      
+          yesterdayHourlySales.forEach(sale => {
+            const { time, totalSalesPriceInTime } = sale;
+            const hour = Number(time.split(' ')[1]); // 시간 부분 추출
+            const index = hour - 10; // 시간에 해당하는 인덱스 계산
+            updatedTimeData[index].어제 = totalSalesPriceInTime; // 해당 시간의 어제 매출액 업데이트
+          });
+      
+          setTimeData(updatedTimeData); // 수정된 timeData 업데이트
+        }
+      }, [todayHourlySales, yesterdayHourlySales, timeData]);
 
     const data = [
         {
@@ -138,7 +240,7 @@ const ManageSale = () => {
             <SideBar />
             <div className={style.mainContainer}>
                 <div>
-                    <div className={style.todaySales}>사장님,<br />오늘 매출은 550,000원 입니다.</div>
+                    <div className={style.todaySales}>사장님,<br />오늘 매출은 {todayTotalSales}원 입니다.</div>
                     <div className={style.salesChartContainer}>
                         <div>
                             <div>시간별 매출</div>
