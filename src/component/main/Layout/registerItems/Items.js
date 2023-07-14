4fa128225a61787f3465e9d9365362612d5a9e54
Anwooseong <aws5624@naver.com>
2023-07-14 23:39:13 +0900

    오늘의 떨이/수정 API 연결(할인율, 수량 넘겨주면 수정 필요)

--- src/component/main/Layout/registerItems/Items.js
+++ src/component/main/Layout/registerItems/Items.js
@@ -1,67 +1,50 @@
 import Input from "../../UI/Input";
-import classes from './Items.module.css'
+import classes from "./Items.module.css";
 
 const Items = (props) => {
-    return (
-        <div className={classes['items-container']}>
-            <div className={classes['title-container']}>
-                <div className={classes['inner-container']}>
-                    <h1 className={classes.title}>{props.title}</h1>
-                    <p className={classes.subTitle}>{props.subTitle}</p>
-                </div>
-            </div>
-            <div className={classes['table-container']}>
-                <table className={classes.type07}>
-                    <thead>
-                        <tr>
-                            <th scope="cols"></th>
-                            <th scope="cols">메뉴명</th>
-                            <th scope="cols">가격</th>
-                            <th scope="cols">할인율</th>
-                            <th scope="cols">수량</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td><Input placeholder='연어 샐러드' /></td>
-                            <td><Input placeholder='연어 샐러드' /></td>
-                            <td><Input placeholder='연어 샐러드' /></td>
-                            <td><Input placeholder='연어 샐러드' /></td>
-                        </tr>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td><Input placeholder='연어 샐러드' /></td>
-                            <td><Input placeholder='연어 샐러드' /></td>
-                            <td><Input placeholder='연어 샐러드' /></td>
-                            <td><Input placeholder='연어 샐러드' /></td>
-                        </tr>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td><Input placeholder='연어 샐러드' /></td>
-                            <td><Input placeholder='연어 샐러드' /></td>
-                            <td><Input placeholder='연어 샐러드' /></td>
-                            <td><Input placeholder='연어 샐러드' /></td>
-                        </tr>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td><Input placeholder='연어 샐러드' /></td>
-                            <td><Input placeholder='연어 샐러드' /></td>
-                            <td><Input placeholder='연어 샐러드' /></td>
-                            <td><Input placeholder='연어 샐러드' /></td>
-                        </tr>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td><Input placeholder='연어 샐러드' /></td>
-                            <td><Input placeholder='연어 샐러드' /></td>
-                            <td><Input placeholder='연어 샐러드' /></td>
-                            <td><Input placeholder='연어 샐러드' /></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+    
+  const menuList = props.menu.map((menu, index) => (
+    <tr key={menu.menuIdx}>
+      <th scope="row">{index + 1}</th>
+      <td>
+        <Input placeholder={menu.menuName} />
+      </td>
+      <td>
+        <Input placeholder={menu.price} />
+      </td>
+      <td>
+        <Input placeholder="0" />
+      </td>
+      <td>
+        <Input placeholder="100" />
+      </td>
+    </tr>
+  ));
+
+  return (
+    <div className={classes["items-container"]}>
+      <div className={classes["title-container"]}>
+        <div className={classes["inner-container"]}>
+          <h1 className={classes.title}>{props.title}</h1>
+          <p className={classes.subTitle}>{props.subTitle}</p>
         </div>
-    )
-}
+      </div>
+      <div className={classes["table-container"]}>
+        <table className={classes.type07}>
+          <thead>
+            <tr>
+              <th scope="cols"></th>
+              <th scope="cols">메뉴명</th>
+              <th scope="cols">가격</th>
+              <th scope="cols">할인율</th>
+              <th scope="cols">수량</th>
+            </tr>
+          </thead>
+          <tbody>{menuList}</tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
 
 export default Items;
--- src/component/main/TodayMenu.js
+++ src/component/main/TodayMenu.js
@@ -1,23 +1,66 @@
-import React from "react";
-import Header from '../header/Header'
-import SideBar from '../sidebar/SideBar'
-import Items from './Layout/registerItems/Items'
-import Button from './UI/Button'
-import classes from './TodayMenu.module.css'
+import React, { useEffect, useState } from "react";
+import Header from "../header/Header";
+import SideBar from "../sidebar/SideBar";
+import Items from "./Layout/registerItems/Items";
+import Button from "./UI/Button";
+import { getCookieToken } from "../../store/common/Cookie";
 
 const Main1 = (props) => {
+    const [menu, setMenu] = useState({
+      mainMenu: null,
+      sideMenu: null,
+    });
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch("https://www.insung.shop/jat/menus", {
+            method: "GET",
+            headers: {
+              "X-ACCESS-TOKEN": getCookieToken(),
+            },
+          });
+          const data = await response.json();
+          if (!data.isSuccess) {
+            console.log(data.message);
+            return;
+          }
+          setMenu({
+            mainMenu: data.result.mainMenuList,
+            sideMenu: data.result.sideMenuList,
+          });
+        } catch (err) {
+          console.log(err);
+        }
+      };
+  
+      fetchData();
+    }, []);
+  
     return (
-        <React.Fragment>
-            <div>
-                <Header />
-                <SideBar />
-                <Items title='메인메뉴 등록' subTitle='오늘 메인메뉴중 떨이 상품으로 등록할 음식들을 할인율과 수량을 입력해주세요' />
-                <Items title='사이드 메뉴 등록' subTitle='오늘 사이드메뉴중 떨이 상품으로 등록할 음식들을 할인율과 수량을 입력해주세요' />
-                <Button />
-            </div>
-        </React.Fragment>
-
-    )
-}
+      <React.Fragment>
+        <div>
+          <Header />
+          <SideBar />
+          {menu.mainMenu && (
+            <Items
+              title="메인메뉴 등록"
+              subTitle="오늘 메인메뉴중 떨이 상품으로 등록할 음식들을 할인율과 수량을 입력해주세요"
+              menu={menu.mainMenu}
+            />
+          )}
+          {menu.sideMenu && (
+            <Items
+              title="사이드 메뉴 등록"
+              subTitle="오늘 사이드메뉴중 떨이 상품으로 등록할 음식들을 할인율과 수량을 입력해주세요"
+              menu={menu.sideMenu}
+            />
+          )}
+          <Button />
+        </div>
+      </React.Fragment>
+    );
+  };
+  
 
 export default Main1;
