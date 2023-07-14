93486e2283856800c9803a6be89467dd56e05ffd
Anwooseong <aws5624@naver.com>
2023-07-15 05:16:36 +0900

    주문표 인쇄 idx리덕스 추가

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './auth'
 import membershipReducer from './membership'
+import printReducer from './print'
 import menuRegisteringReducer from "./menuRegisteringSlice";
 
 export default configureStore({
     reducer: {
         auth: authReducer,
         menuRegistering: menuRegisteringReducer,
-        membership: membershipReducer
+        membership: membershipReducer,
+        print: printReducer
     },
 })
